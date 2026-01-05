be27c1befaec9cf0ac70df6948542858fad721c0
tksshj <tksshj@gmail.com>
2026-01-06 06:44:15 +0900

    tinko

--- app/atelier/plane-geometry/_components/Canvas.tsx
+++ app/atelier/plane-geometry/_components/Canvas.tsx
@@ -13,13 +13,8 @@ export default function Canvas() {
     if (!mount) {
       return
     }
-    const width = mount.clientHeight
-    const height = width
-
-    const renderer = new THREE.WebGLRenderer({ alpha: false, antialias: true })
+    const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true })
     renderer.setSize(WIDTH, HEIGHT, false)
-    // renderer.setPixelRatio(window.devicePixelRatio)
-    renderer.setClearColor(0xffffff, 1)
     mount.appendChild(renderer.domElement)
 
     const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.01, 10)
@@ -40,51 +35,13 @@ export default function Canvas() {
       `,
     })
 
-    const geometry = new THREE.PlaneGeometry(2, 2)
+    const geometry = new THREE.PlaneGeometry(0.1, 0.1)
 
-    const mesh = new THREE.Mesh(geometry, material)
-    scene.add(mesh)
-
-    const material2 = new THREE.ShaderMaterial({
-      vertexShader: `
-        varying vec2 vUv;
-
-        void main() {
-          vUv = uv;
-          gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-        }
-      `,
-      fragmentShader: `
-        varying vec2 vUv;
-
-      void main() {
-  vec2 p = vUv - 0.5;
-  float d = length(p);
-
-  float r = 0.25;
-  float edge = 0.002;
-
-  float c = smoothstep(0.1, 0.3, d);
-
-  gl_FragColor = vec4(vec3(c), d);
-
-  // vec2 p = vUv - 0.5;          // 中心を (0,0) に
-  // float d = length(p);         // 中心からの距離
-
-  // float r = 0.25;              // 半径
-  // float c = step(d, r);        // 円の内側 = 1
-
-  // gl_FragColor = vec4(vec3(c), 1.0);
-      // gl_FragColor = vec4(0.1627, 0.3098, 0.4000, 1.0) * length(vUv);
-      //      gl_FragColor = vec4(vUv, 0.0, 1.0);
-        }
-      `,
-    })
-
-    const geometry2 = new THREE.PlaneGeometry(0.5, 0.5)
-
-    const mesh2 = new THREE.Mesh(geometry2, material2)
-    scene.add(mesh2)
+    for (let i = 0; i < 32; i++) {
+      const mesh = new THREE.Mesh(geometry, material)
+      mesh.position.set(Math.random() * 2.0 - 1.0, Math.random() * 2.0 - 1.0, 0)
+      scene.add(mesh)
+    }
 
     let rafId = 0
     let running = true
@@ -120,11 +77,3 @@ export default function Canvas() {
     />
   )
 }
-
-// <Typography variant='p
-//       <pre>
-//     const geometry = new THREE.PlaneGeometry(2, 2)
-
-//   const geometry = new THREE.BufferGeometry()
-//   geometry.setAttribute('position', new THREE.Float32BufferAttribute([-1, -1, 0, 1, -1, 0, 1, 1, 0, -1, 1, 0], 3))
-//   geometry.setIndex([0, 1, 2, 0, 2, 3])
