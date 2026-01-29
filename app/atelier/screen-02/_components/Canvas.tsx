7d3f94a302b6a784596dbbd9bca58341958c8ebf
tksshj <tksshj@gmail.com>
2026-01-29 23:01:53 +0900

    tinko

--- app/atelier/screen-02/_components/Canvas.tsx
+++ app/atelier/screen-02/_components/Canvas.tsx
@@ -5,7 +5,7 @@ import * as THREE from 'three'
 const WIDTH = 1024
 const HEIGHT = 1024
 
-export default function Canvas({ w, h }: { w: number; h: number }) {
+export default function Canvas({ w, h, nPoints }: { w: number; h: number; nPoints: number }) {
   const mountRef = useRef<HTMLDivElement | null>(null)
 
   useEffect(() => {
@@ -13,6 +13,17 @@ export default function Canvas({ w, h }: { w: number; h: number }) {
     if (!mount) {
       return
     }
+
+    const aspect = w / h
+    const cols = Math.floor(Math.sqrt(nPoints * aspect))
+    const rows = Math.floor(nPoints / cols)
+
+    console.log(w)
+    console.log(h)
+    console.log(nPoints)
+    console.log(aspect)
+    console.log(cols)
+    console.log(rows)
 
     const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true })
     renderer.setSize(WIDTH, HEIGHT, false)
@@ -53,11 +64,56 @@ export default function Canvas({ w, h }: { w: number; h: number }) {
         }
       `,
     })
-
     const geometry = new THREE.PlaneGeometry(2, 2)
-
     const mesh = new THREE.Mesh(geometry, material)
     scene.add(mesh)
+
+    const positions = new Float32Array(cols * rows * 3)
+    let idx = 0
+    for (let y = 0; y < rows; y++) {
+      for (let x = 0; x < cols; x++) {
+        const px = ((x + 0.5) * w) / cols
+        const py = ((y + 0.5) * h) / rows
+        const worldX = px / w
+        const worldY = py / h
+        positions[idx++] = worldX
+        positions[idx++] = worldY
+        positions[idx++] = 0.0 // z
+      }
+    }
+    const pointsGeometry = new THREE.BufferGeometry()
+    pointsGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+
+    const pointsMaterial = new THREE.ShaderMaterial({
+      transparent: true,
+      depthTest: false,
+      depthWrite: false,
+      uniforms: {
+        uResolution: {
+          value: new THREE.Vector2(w, h),
+        },
+      },
+      vertexShader: `
+        void main() {
+          vec3 pos = (position - 0.5) * 2.0;
+          gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
+          gl_PointSize = 20.0;
+        }
+      `,
+      fragmentShader: `
+        void main() {
+          vec2 uv = gl_PointCoord - 0.5;
+          if (length(uv) > 0.5) {
+             discard;
+          }
+          float shape = smoothstep(0.5, 0.0, length(uv));
+          gl_FragColor = vec4(1.0, 0.0, 0.0, shape);
+        }
+      `,
+    })
+
+    const pointsMesh = new THREE.Points(pointsGeometry, pointsMaterial)
+    scene.add(pointsMesh)
 
     let rafId = 0
     let running = true
@@ -78,7 +134,7 @@ export default function Canvas({ w, h }: { w: number; h: number }) {
       material.dispose()
       mount.removeChild(renderer.domElement)
     }
-  }, [w, h])
+  }, [w, h, nPoints])
 
   return (
     <Box
--- app/atelier/screen-02/_components/PageContent.tsx
+++ app/atelier/screen-02/_components/PageContent.tsx
@@ -12,21 +12,21 @@ export default function PageContent() {
 
   return (
     <Box sx={{ width: '100%', padding: '20px 0 0' }}>
-      <Canvas w={wPos * 1920 * 0.01} h={hPos * 1920 * 0.01} />
+      <Canvas w={wPos * 1920 * 0.01} h={hPos * 1920 * 0.01} nPoints={nPoints * 0.01 * 8192} />
       <Box sx={{ margin: '40px 0 20px', display: 'flex', alignItems: 'center' }}>
-        <Slider value={wPos} onChange={(_, newValue) => setWPos(newValue)} sx={{ flex: '0 0 90%' }} />
+        <Slider value={wPos} onChange={(_, newValue) => setWPos(newValue)} sx={{ flex: '1 1 90%' }} />
         <Typography component='p' variant='h1' sx={{ flex: '1 1 30%', textAlign: 'right' }}>
           {wPos * 0.01 * 1920}
         </Typography>
       </Box>
       <Box sx={{ margin: '20px 0', display: 'flex', alignItems: 'center' }}>
-        <Slider value={hPos} onChange={(_, newValue) => setHPos(newValue)} sx={{ flex: '0 0 90%' }} />
+        <Slider value={hPos} onChange={(_, newValue) => setHPos(newValue)} sx={{ flex: '1 1 90%' }} />
         <Typography component='p' variant='h1' sx={{ flex: '1 1 30%', textAlign: 'right' }}>
           {hPos * 0.01 * 1920}
         </Typography>
       </Box>
       <Box sx={{ display: 'flex', alignItems: 'center' }}>
-        <Slider value={nPoints} onChange={(_, newValue) => setNPoints(newValue)} sx={{ flex: '0 0 90%' }} />
+        <Slider value={nPoints} onChange={(_, newValue) => setNPoints(newValue)} sx={{ flex: '1 1 90%' }} />
         <Typography component='p' variant='h1' sx={{ flex: '1 1 30%', textAlign: 'right' }}>
           {Math.round(nPoints * 0.01 * 8192)}
         </Typography>
