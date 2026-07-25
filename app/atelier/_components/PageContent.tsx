35475a01ae46ed1187507510ef99ef59338dcc76
tksshj <tksshj@gmail.com>
2026-07-25 09:11:32 +0900

    tinko

--- app/atelier/_components/PageContent.tsx
+++ app/atelier/_components/PageContent.tsx
@@ -7,33 +7,31 @@ import index from './index.json'
 
 export default function PageContent() {
   return (
-    <Box>
-      <Box component='ul' sx={{ listStyle: 'none', padding: '0', margin: '0' }}>
-        {index.map((item) => (
-          <MuiLink key={item.title} component={Link} href={item.path} color='inherit' underline='hover'>
-            <Box
-              component='li'
-              sx={{
-                padding: '12px 0',
-                display: { xs: 'block', sm: 'flex' },
-                alignItems: 'center',
-                gap: '8px',
-                borderBottom: (theme) => `solid 1px ${theme.palette.divider}`,
-                '&:hover': {
-                  backgroundColor: (theme) => theme.palette.action.hover,
-                },
-              }}
-            >
-              <Typography component='h4' variant='h4' sx={{ flex: '0 0 256px' }}>
-                {item.title}
-              </Typography>
-              <Typography component='p' variant='body1' sx={{ fontSize: { xs: '12px', sm: '14px' } }}>
-                {item.description}
-              </Typography>
-            </Box>
-          </MuiLink>
-        ))}
-      </Box>
+    <Box component='ul' sx={{ listStyle: 'none', padding: '0', margin: '0' }}>
+      {index.map((item) => (
+        <MuiLink key={item.title} component={Link} href={item.path} color='inherit' underline='hover'>
+          <Box
+            component='li'
+            sx={{
+              padding: '12px 0',
+              display: { xs: 'block', sm: 'flex' },
+              alignItems: 'center',
+              gap: '8px',
+              borderBottom: (theme) => `solid 1px ${theme.palette.divider}`,
+              '&:hover': {
+                backgroundColor: (theme) => theme.palette.action.hover,
+              },
+            }}
+          >
+            <Typography component='h4' variant='h4' sx={{ flex: '0 0 256px' }}>
+              {item.title}
+            </Typography>
+            <Typography component='p' variant='body1' sx={{ fontSize: { xs: '12px', sm: '14px' } }}>
+              {item.description}
+            </Typography>
+          </Box>
+        </MuiLink>
+      ))}
     </Box>
   )
 }
